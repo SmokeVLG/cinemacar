1364baa3451b2ead4ccebc8c7ff1a8c9cfd2d461
maxden33@gmail.com <maxden33@gmail.com>
2019-05-09 09:50:09 +0300

    Initial commit

---
+++ app/src/main/java/com/cinemacar/activities/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.cinemacar.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cinemacar.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        goToMap();
+    }
+
+    private void goToMap() {
+        startActivity(new Intent(this, MainActivity.class));
+        this.finish();
+    }
+
+}
---
+++ app/src/main/java/com/cinemacar/pojo/Film.java
@@ -0,0 +1,23 @@
+package com.cinemacar.pojo;
+
+public class Film {
+
+	private String date;
+	private String description;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
