c33f68e310e0c3408e0a71a2833053d305187e4a
SmokeVLG <maxden33@gmail.com>
2019-05-28 12:52:39 +0400

    Рефакторинг.

--- app/src/main/java/com/cinemacar/App.java
+++ app/src/main/java/com/cinemacar/App.java
@@ -5,12 +5,20 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(mailTo = "maxden33@gmail.com",
+		mode = ReportingInteractionMode.TOAST,
+		resToastText = R.string.crash_toast_text)
 public class App extends Application {
 	private static App ourInstance;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ACRA.init(this);
 		if (ourInstance == null) ourInstance = this;
 	}
 
